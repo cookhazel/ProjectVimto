6501cbbbfb90ed90d3406bb251e2221e64763ed2
lukebrown0 <luke-brown@hotmail.co.uk>
2013-12-03 12:44:53 +0000

    tabs have a buttons that dynamically update

--- LukeClasses/doctorsSurgery/MainUI.Designer.cs
+++ LukeClasses/doctorsSurgery/MainUI.Designer.cs
@@ -28,42 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btBack_Click = new System.Windows.Forms.Button();
-            this.btSubmit_click = new System.Windows.Forms.Button();
             this.tabs = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btSubmit = new System.Windows.Forms.Button();
+            this.btBack = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.button1 = new System.Windows.Forms.Button();
             this.tabs.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // btBack_Click
-            // 
-            this.btBack_Click.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.btBack_Click.Location = new System.Drawing.Point(508, 4);
-            this.btBack_Click.Name = "btBack_Click";
-            this.btBack_Click.Size = new System.Drawing.Size(75, 23);
-            this.btBack_Click.TabIndex = 3;
-            this.btBack_Click.Text = "Back";
-            this.btBack_Click.UseVisualStyleBackColor = true;
-            this.btBack_Click.Click += new System.EventHandler(this.btBack_Click_Click);
-            // 
-            // btSubmit_click
-            // 
-            this.btSubmit_click.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.btSubmit_click.Location = new System.Drawing.Point(589, 4);
-            this.btSubmit_click.Name = "btSubmit_click";
-            this.btSubmit_click.Size = new System.Drawing.Size(75, 23);
-            this.btSubmit_click.TabIndex = 4;
-            this.btSubmit_click.Text = "Submit";
-            this.btSubmit_click.UseVisualStyleBackColor = true;
-            this.btSubmit_click.Click += new System.EventHandler(this.btSubmit_click_Click);
             // 
             // tabs
             // 
@@ -72,7 +48,7 @@
             this.tabs.Location = new System.Drawing.Point(0, 0);
             this.tabs.Name = "tabs";
             this.tabs.SelectedIndex = 0;
-            this.tabs.Size = new System.Drawing.Size(678, 371);
+            this.tabs.Size = new System.Drawing.Size(678, 395);
             this.tabs.TabIndex = 2;
             // 
             // tabPage1
@@ -81,11 +57,40 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(670, 345);
+            this.tabPage1.Size = new System.Drawing.Size(670, 369);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "hometab";
             this.tabPage1.UseVisualStyleBackColor = true;
             this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btSubmit);
+            this.panel1.Controls.Add(this.btBack);
+            this.panel1.Location = new System.Drawing.Point(3, 332);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(661, 34);
+            this.panel1.TabIndex = 7;
+            // 
+            // btSubmit
+            // 
+            this.btSubmit.Location = new System.Drawing.Point(504, 4);
+            this.btSubmit.Name = "btSubmit";
+            this.btSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btSubmit.TabIndex = 1;
+            this.btSubmit.Text = "Submit";
+            this.btSubmit.UseVisualStyleBackColor = true;
+            this.btSubmit.Click += new System.EventHandler(this.btSubmit_Click);
+            // 
+            // btBack
+            // 
+            this.btBack.Location = new System.Drawing.Point(585, 4);
+            this.btBack.Name = "btBack";
+            this.btBack.Size = new System.Drawing.Size(75, 23);
+            this.btBack.TabIndex = 0;
+            this.btBack.Text = "Back";
+            this.btBack.UseVisualStyleBackColor = true;
+            this.btBack.Click += new System.EventHandler(this.btBack_Click);
             // 
             // label1
             // 
@@ -97,45 +102,22 @@
             this.label1.Text = "PLEASE DELETE. /T CHANGE THE FORM SIZE TO \"695, 449\"";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // panel1
-            // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.BackColor = System.Drawing.Color.Transparent;
-            this.panel1.Controls.Add(this.btSubmit_click);
-            this.panel1.Controls.Add(this.btBack_Click);
-            this.panel1.Location = new System.Drawing.Point(4, 373);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(670, 63);
-            this.panel1.TabIndex = 5;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
-            // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.panel1);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(670, 384);
+            this.tabPage2.Size = new System.Drawing.Size(670, 369);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(752, 254);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // MainUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(838, 438);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(836, 423);
             this.Controls.Add(this.tabs);
             this.Name = "MainUI";
             this.Text = "Form1";
@@ -144,20 +126,20 @@
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
             this.panel1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btBack_Click;
-        private System.Windows.Forms.Button btSubmit_click;
         private System.Windows.Forms.TabPage tabPage2;
         protected System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.Panel panel1;
         public System.Windows.Forms.Label label1;
         protected System.Windows.Forms.TabControl tabs;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btSubmit;
+        private System.Windows.Forms.Button btBack;
     }
 }
 
--- LukeClasses/doctorsSurgery/MainUI.cs
+++ LukeClasses/doctorsSurgery/MainUI.cs
@@ -15,30 +15,15 @@ namespace doctorsSurgery
         public MainUI()
         {
             InitializeComponent();
-            
+            userControl(tabs.SelectedTab); 
+            tabs.Selecting += new TabControlCancelEventHandler(tabs_Selecting);
         }
 
-        public void OnTabIndexChanged(object sender, EventArgs e)
-        {
-            tabPage2.Controls.Add(panel1);
-        }
 
-        public void btBack_Click_Click(object sender, EventArgs e)
+        void tabs_Selecting(object sender, TabControlCancelEventArgs e)
         {
-            /* --Back Button. Also used when opening up new forms--
-             * 
-             * FormX object1 = new FormX();  //replace FormX with the form you wish to open
-             * FormX.RefTo(Current) = this;  //replace (Current) with the form that you are on
-             * FormX.Show();                 //starts FormX
-             * this.Close();                 //closes the current form
-             * 
-             */
-        }
-
-        public void btSubmit_click_Click(object sender, EventArgs e)
-        {
-            //databaseMethod(textBox1.Text, textbox2.Text ect ect);
-            
+            TabPage current = (sender as TabControl).SelectedTab;
+            userControl(tabs.SelectedTab); 
         }
 
         protected void tabPage1_Click(object sender, EventArgs e)
@@ -58,12 +43,45 @@ namespace doctorsSurgery
 
         private void label1_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void MainUI_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void userControl(TabPage tabpage_NO)
+        {
+            tabpage_NO.Controls.Add(panel1);
+        }
+
+
+       
+
+
+        
+
+        private void btBack_Click(object sender, EventArgs e)
+        {
+            /* --Back Button. Also used when opening up new forms--
+             * 
+             * FormX object1 = new FormX();  //replace FormX with the form you wish to open
+             * FormX.RefTo(Current) = this;  //replace (Current) with the form that you are on
+             * FormX.Show();                 //starts FormX
+             * this.Close();                 //closes the current form
+             * 
+             */
+        }
+
+        private void btSubmit_Click(object sender, EventArgs e)
+        {
+            //databaseMethod(textBox1.Text, textbox2.Text ect ect);
         }
 
 

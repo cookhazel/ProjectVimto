52a8bb2f55dcc85c63acfcfd877bdf0e3d9fde1b
KyleGriffin <kyle.griffin@hotmail.co.uk>
2014-01-03 19:54:50 +0000

    Added cs file and designer.
    Not sure if missing something... Looks like it is. I'll update them
    shortly

---
+++ KyleClasses/MainMenuUI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainMenuUI
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        public void btnPatient_Click(object sender, EventArgs e)
+        {
+            // Takes you to find patient form, which I'm in the process of doing
+            PatientForm.FormFindPatient.Show();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Log in form comes up here
+            this.Close();
+        }
+
+        private void MainMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelChangeApp_Click(object sender, EventArgs e)
+        {
+// takes user to appoinment form so appointments can be changed or deleted
+        }
+    }
+}
---
+++ KyleClasses/PatientForm.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace PatientForm
+{
+    
+    public partial class FormFindPatient : Form
+    {
+        public class patient
+        {
+            public string patientID;
+            public string name;
+            public string DOB;
+            public string address;
+        }
+        public FormFindPatient()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        { 
+      
+            patient patient1 = new patient();
+            Convert.ToString(txtpatientID);
+            patient1.patientID = txtpatientID.Text;
+
+            Convert.ToString(txtname);
+            patient1.name = txtname.Text;
+
+            patient1.DOB = string.Format("{0:dd/mm/yyyy}", DTdob.Value.ToShortDateString());
+             
+
+            Convert.ToString(txtaddress); 
+            patient1.address = txtaddress.Text;
+
+            
+
+            MessageBox.Show("Here is what you have entered: " + patient1.patientID + " " + patient1.name + " " + patient1.address + " " + patient1.DOB);
+        }
+
+        private void txtpatientID_TextChanged(object sender, EventArgs e)
+        {
+            txtname.Enabled = false;
+            DTdob.Enabled = false;
+            txtaddress.Enabled = false;
+
+            if (txtpatientID.Text == string.Empty)
+            {
+                txtname.Enabled = true;
+                DTdob.Enabled = true;
+                txtaddress.Enabled = true;
+            }
+        }
+
+        private void txtname_TextChanged(object sender, EventArgs e)
+        {
+            txtpatientID.Enabled = false;
+            lbl.Text = "Date of Birth OR Address also required";
+            if (txtname.Text == string.Empty)
+            {
+                lbl.Text = "";
+                txtpatientID.Enabled = true;
+            }
+        }
+
+        private void txtaddress_TextChanged(object sender, EventArgs e)
+        {
+            DTdob.Enabled = false;
+            txtpatientID.Enabled = false;
+            if (txtaddress.Text == string.Empty)
+            {
+                DTdob.Enabled = true;
+                txtpatientID.Enabled = true;
+            }
+        }
+
+        private void DTdob_ValueChanged(object sender, EventArgs e)
+        {
+            
+           
+        }
+    }
+    
+
+}
